cc66791cad65b92cdee57e5740c939afbb67324d
Antoniobox <antonioboxsanchez@gmail.com>
2023-05-10 10:23:07 +0200

    Agregado contenido para habitaciones. Agregados métodos para guardar registros y leerlos en GestorHabitaciones

--- Controllers/GestorHabitaciones.java
+++ Controllers/GestorHabitaciones.java
@@ -1,7 +1,9 @@
 package Controllers;
 
+import Exceptions.InvalidFieldsHabitacion;
 import Models.Habitacion;
 
+import java.io.*;
 import java.util.ArrayList;
 
 /**
@@ -19,6 +21,20 @@ public class GestorHabitaciones {
 
     public GestorHabitaciones(ArrayList<Habitacion> habitaciones){
         this.habitaciones = habitaciones;
+    }
+
+    //TODO reformular el bloque try-catch
+    public GestorHabitaciones(ArrayList<Habitacion> habitaciones, boolean cargarBD){
+        this.habitaciones = habitaciones;
+        try{
+            cargarBaseDeDatos();
+        }catch(FileNotFoundException fnfe){
+            System.out.println("No se encuentra el fichero de la base de datos de habitacion");
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }catch(InvalidFieldsHabitacion ifh){
+            ifh.printStackTrace();
+        }
     }
     public ArrayList<Habitacion> getHabitaciones() {
         return habitaciones;
@@ -44,5 +60,44 @@ public class GestorHabitaciones {
         habitaciones.add(habitacion);
     }
 
+    /**
+     * Carga el fichero "habitaciones.dat", donde se encuentran los registros de las habitaciones, a la lista de habitaciones
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws InvalidFieldsHabitacion
+     */
+    private void cargarBaseDeDatos() throws FileNotFoundException, IOException, InvalidFieldsHabitacion {
+        FileReader fr = new FileReader("Data/habitaciones");
+        BufferedReader br = new BufferedReader(fr);
 
+        String linea;
+        String[] registro;
+
+        while((linea=br.readLine())!=null){
+            registro = linea.split(";");
+            int id, num_camas, max_personas;
+            String nombre, descripcion, fechasOcupadas;
+            double precio;
+            id = Integer.parseInt(registro[0]);
+            nombre = registro[1];
+            descripcion = registro[2];
+            try{
+                num_camas = Integer.parseInt(registro[3]);
+                max_personas = Integer.parseInt(registro[4]);
+                precio = Double.parseDouble(registro[5]);
+            }catch(NumberFormatException e){
+                throw new InvalidFieldsHabitacion("Existen campos incorrectos en el registro");
+            }
+            habitaciones.add(new Habitacion(id, nombre, descripcion, num_camas, max_personas, precio));
+        }
+    }
+
+    public void guardarRegistros() throws IOException{
+        FileWriter fw = new FileWriter("Data/habitaciones.dat");
+        if(habitaciones.size() > 0){
+            for(Habitacion h : habitaciones){
+                //TODO pensar si comprobar si las habitaciones deben de tener todos los campos con algun valor para escribirlas
+            }
+        }
+    }
 }
--- Models/Habitacion.java
+++ Models/Habitacion.java
@@ -13,7 +13,6 @@ public class Habitacion {
 	private String descripcion;
 	private int num_camas;
 	private int max_personas;
-	private boolean ocupada;
 	private String fechasOcupadas="";
 
 
@@ -22,13 +21,12 @@ public class Habitacion {
 	public Habitacion() {
 	}
 
-	public Habitacion(int id, String nombre, String descripcion, int num_camas, int max_personas, boolean ocupada, double precio) {
+	public Habitacion(int id, String nombre, String descripcion, int num_camas, int max_personas, double precio) {
 		this.id = id;
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 		this.num_camas = num_camas;
 		this.max_personas = max_personas;
-		this.ocupada = ocupada;
 		this.precio = precio;
 	}
 
@@ -72,13 +70,6 @@ public class Habitacion {
 		this.max_personas = max_personas;
 	}
 
-	public boolean isOcupada() {
-		return ocupada;
-	}
-
-	public void setOcupada(boolean ocupada) {
-		this.ocupada = ocupada;
-	}
 
 	public double getPrecio() {
 		return precio;
@@ -105,14 +96,15 @@ public class Habitacion {
 				", descripcion='" + descripcion + '\'' +
 				", num_camas=" + num_camas +
 				", max_personas=" + max_personas +
-				", ocupada=" + ocupada +
 				", fechasOcupadas='" + fechasOcupadas + '\'' +
 				", precio=" + precio +
 				'}';
 	}
 
 	/**
-	 *
+	 * Devuelve un ArrayList con los ID's que contiene la opción que ha elegido el usuario
+	 * @param ids String con los ids de las habitaciones usando como separador el ;
+	 * @return ArrayList con los ID's
 	 */
 	public static ArrayList<Integer> getIdsListado(String ids){
 		ArrayList<Integer> aIds = new ArrayList<>();
@@ -127,11 +119,11 @@ public class Habitacion {
 	 */
 	public static ArrayList<Habitacion> generarHabitacionesBase(){
 		ArrayList<Habitacion> habitaciones = new ArrayList<>();
-		habitaciones.add(new Habitacion((int)(Math.random()*1000+1), "A1", "habitacion con vistas a la ciudad", 4, 8, false, 80.50));
-		habitaciones.add(new Habitacion((int)(Math.random()*1000+1), "A2", "habitacion con vistas a la piscina", 2, 3, false, 40.75));
-		habitaciones.add(new Habitacion((int)(Math.random()*1000+1), "A3", "habitacion con vistas al campo", 1, 1, false, 70.00));
-		habitaciones.add(new Habitacion((int)(Math.random()*1000+1), "A4", "habitacion con vistas a un descampado", 2, 4, false, 75.00));
-		habitaciones.add(new Habitacion((int)(Math.random()*1000+1), "A5", "habitacion con vistas a la montaña", 3, 5, false, 60.30));
+		habitaciones.add(new Habitacion((int)(Math.random()*1000+1), "A1", "habitacion con vistas a la ciudad", 4, 8, 80.50));
+		habitaciones.add(new Habitacion((int)(Math.random()*1000+1), "A2", "habitacion con vistas a la piscina", 2, 3, 40.75));
+		habitaciones.add(new Habitacion((int)(Math.random()*1000+1), "A3", "habitacion con vistas al campo", 1, 1, 70.00));
+		habitaciones.add(new Habitacion((int)(Math.random()*1000+1), "A4", "habitacion con vistas a un descampado", 2, 4, 75.00));
+		habitaciones.add(new Habitacion((int)(Math.random()*1000+1), "A5", "habitacion con vistas a la montaña", 3, 5, 60.30));
 		return habitaciones;
 	}
 
